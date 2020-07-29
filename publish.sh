05ac727118d1074f7a27a68467888fecdd6f7cdb
Matt Gaunt <mattgaunt@google.com>
2020-07-28 21:01:02 -0700

    Changing deploy time and update publish script to match web-push-book

--- publish.sh
+++ publish.sh
@@ -9,20 +9,17 @@ aws s3 sync ./public/ s3://www.gauntface.com/ --delete --acl public-read
 
 aws s3 cp \
 s3://www.gauntface.com/ s3://www.gauntface.com/ \
---metadata-directive REPLACE \
---cache-control 'max-age=86400' \
 --exclude="*" \
 --include="*.html" \
 --include="*.xml" \
 --include="*.json" \
 --include="*.svg" \
+--cache-control 'max-age=86400' \
 --acl public-read \
 --recursive
 
 aws s3 cp \
 s3://www.gauntface.com/ s3://www.gauntface.com/ \
---metadata-directive REPLACE \
---cache-control 'max-age=31104000' \
 --exclude="*" \
 --include="*.css" \
 --include="*.js" \
@@ -33,5 +30,6 @@ s3://www.gauntface.com/ s3://www.gauntface.com/ \
 --include="*.webp" \
 --include="*.woff" \
 --include="*.woff2" \
+--cache-control 'max-age=31104000' \
 --acl public-read \
 --recursive
